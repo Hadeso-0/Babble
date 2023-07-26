f657412382a80529486966db8b29acc1126d7786
Manav <manavagarwal1410@gmail.com>
2023-07-26 19:14:11 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -77,6 +77,9 @@ app.use(
 app.use(mongosanitize());
 app.use(xss());
 app.use("/api", routes);
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
 
 const server = http.createServer(app);
 connectDB();
@@ -88,19 +91,19 @@ process.on("uncaughtException", (err) => {
 });
 
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 
 const port = process.env.PORT || 5000;
