2da32a7421857186093e8a7056d49232b24be5b4
Manav <manavagarwal1410@gmail.com>
2023-07-26 19:20:32 +0530

    env setup frontend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,8 @@
 import io from "socket.io-client";
 
 let socket;
-// process.env.SERVER_URL || 
 const connectSocket = () => {
-  socket = io("http://localhost:5004");
+  socket = io(process.env.SERVER_URL);
 }
 
 export { socket, connectSocket };
